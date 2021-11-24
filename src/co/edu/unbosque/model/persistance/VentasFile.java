b5e5b1f0cf0528a94098c47f3c8f7b4d30aee295
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2021-11-24 13:30:30 -0500

    añadiendo ventasfile

--- src/co/edu/unbosque/model/persistance/VentasFile.java
+++ src/co/edu/unbosque/model/persistance/VentasFile.java
@@ -1,5 +1,76 @@
 package co.edu.unbosque.model.persistance;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import co.edu.unbosque.model.ClienteDTO;
+import co.edu.unbosque.model.JugadorDTO;
+import co.edu.unbosque.model.ProductoDTO;
+import co.edu.unbosque.model.VentaDTO;
+
 public class VentasFile {
+	private String ruta1="./Data/cliente.out";
+	private String ruta2="./Data/producto.out";
+	private File f;
+	private FileOutputStream fos;     
+	private DataOutputStream dos;
+	private FileInputStream fis;     
+	private DataInputStream dis;
+	private ClienteDTO cliente;
+	private ProductoDTO venta;
+	
+	public int escribirArchivoproducto(ArrayList<ProductoDTO> Staff_productos) {
+		try {
+			ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(ruta2));
+			salida.writeObject(Staff_productos);
+			salida.close();
+		}catch(IOException e) {
+			return -1;
+		}
+		return 0;
+	}
+	public ArrayList<ProductoDTO> leerArchivoEmpleado() {
+		ArrayList<ProductoDTO> nomina = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(ruta2));
+			nomina = (ArrayList<ProductoDTO>)in.readObject();
+			in.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+		return nomina;
+	}
+	
+	public int escribirArchivocliente(ArrayList<ClienteDTO> Staff_clientes) {
+		try {
+			ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(ruta1));
+			salida.writeObject(Staff_clientes);
+			salida.close();
+		}catch(IOException e) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	public ArrayList<ClienteDTO> leerArchivocliente() {
+		ArrayList<ClienteDTO> nomina = null;
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(ruta1));
+			nomina = (ArrayList<ClienteDTO>)in.readObject();
+			in.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+		return nomina;
+	}
 
 }
